414cad937d9bbb6af833ba34cf38728b67545ec3
unknown <sudhakargutti.sudha@gmail.com>
2023-06-04 12:05:47 +0530

    restful crud operations with db

---
+++ Rest-Service/spring-boot-restful-webservice/src/test/java/com/realtech/SpringBootRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
