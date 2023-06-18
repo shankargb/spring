2dc201f9be787af83c6a65b220c65058da4df812
unknown <sudhakargutti.sudha@gmail.com>
2023-06-18 12:06:39 +0530

    service registry and api-gateway enhancemnet

--- Microservices/employee-service/src/main/java/com/realtech/employeeservice/EmployeeServiceApplication.java
+++ Microservices/employee-service/src/main/java/com/realtech/employeeservice/EmployeeServiceApplication.java
@@ -1,14 +1,17 @@
 package com.realtech.employeeservice;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
